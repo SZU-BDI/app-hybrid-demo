d241683fab6ff70c16e03b926a2099a0694e7784
cmptech <wanjochan@gmail.com>
2016-06-13 01:07:22 +0800

    reorganize with the config and assets dir

--- proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/DemoApp.java
+++ proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/DemoApp.java
@@ -2,8 +2,14 @@ package szu.bdi.hybrid.demo;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.Build;
 import android.os.StrictMode;
 import android.util.Log;
+
+import org.json.JSONObject;
+
+import szu.bdi.hybrid.core.HybridTools;
+import szu.bdi.hybrid.core.HybridUi;
 
 //import com.hybrid.core.UiRoot;
 
@@ -15,95 +21,55 @@ public class DemoApp extends Application {
         }
     }.getClassName());
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        final Context _ctx = //AppHelper.appContext =
-                getApplicationContext();
-//        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
-//            AppHelper.showMsg(getApplicationContext(),
-//                    "Your Phone is too old...May have some problem");
-//        }
-
-//        Intent intent = new Intent(_ctx, UiRoot.class);
-//
-////        intent.putExtra(Extra_openwebsite_key, text);
-////        intent.setData(url);
-//        _ctx.startActivity(intent);
-
-//        Intent bg = new Intent(_ctx, UiRoot.class);
-//        this.startService(bg);
-        //TODO have to run a backgroup service to check network from time to time...
-        //NOTES: for main thread (could) using network, can do a policy config hack..
+    public static void main() {
         int _sdk_int = android.os.Build.VERSION.SDK_INT;
+
+        if (_sdk_int < Build.VERSION_CODES.KITKAT) {
+            HybridTools.quickShowMsgMain("Your Phone is too old (API " + _sdk_int + ")... May not stable");
+        }
+
+        //NOTES: for main thread (could) using network, do a policy hack config
         if (_sdk_int > 9) {
             Log.d(LOGTAG, "setThreadPolicy for " + _sdk_int);
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
+
+        //get the config from assets
+
+        String sJsonConf = HybridTools.assetFile2Str("config.json");
+        Log.v(LOGTAG, "config.json=" + sJsonConf);
+        JSONObject o = HybridTools.s2o(sJsonConf);
+        Log.v(LOGTAG, "config.json.o=" + o);
+        if (!HybridTools.isEmptyString(o.optString("ermsg"))) {
+            HybridTools.quickShowMsgMain("Wrong Config File");
+            HybridTools.quit(false);
+        } else {
+            HybridTools.jsonConfig = o;
+        }
+
+        //Start a UI (UiRoot) in full screen but bar.
+        HybridUi ui = HybridTools.getHybridUi("UiRoot");
+        //TODO style..
+        //ui.style=STYLE_FULL_WITH_HEAD;
+        //TODO load uri
+        //"file://android_asset/root.htm"
+        HybridTools.showUi(ui, null);
+
+//TODO
+// to run a backgroup service to check network
+//Intent bg = new Intent(getApplicationContext(), DemoBackgroundService.class);
+//this.startService(bg);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //IMPORTANT...STORE the app context into the hybrid service for later use.
+        Context _appContext = getApplicationContext();
+        HybridTools.setAppContext(_appContext);
+
         Log.v(LOGTAG, "Application.onCreate");
-
-//        Handler handler = new Handler(Looper.getMainLooper());
-//        handler.postDelayed(new Runnable() {
-//            public void run() {
-//                Intent intent = new Intent(_ctx, HybridUi.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(intent);
-////                HybridService hs = new HybridService(_ctx);
-////                HybridUi ui = hs.getHybridUi("file://android_asset/root.htm");
-////                HybridService.getHybridUi(_ctx, "file://android_asset/root.htm");
-//            }
-//        }, 10);
-
-//        Context _ctx = getApplicationContext();
-
-//        new Handler().postDelayed(new Runnable() {
-//            public void run() {
-////                Intent intent = new Intent(_ctx, HybridUi.class);
-////                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-////                startActivity(intent);
-//                HybridService hs = new HybridService(_ctx);
-//                HybridUi ui = hs.getHybridUi("file://android_asset/root.htm");
-////                ui.show();TODO
-////                HybridService.getHybridUi(_ctx, "file://android_asset/root.htm");
-//            }
-//        }, 1);
-//
-//        new Handler().postDelayed(new Runnable() {
-//            public void run() {
-//                Intent intent = new Intent(_ctx, UiContent.class);
-//                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                startActivity(intent);
-////                HybridService hs = new HybridService(_ctx);
-////                HybridUi ui = hs.getHybridUi("file://android_asset/root.htm");
-////                ui.show();TODO
-////                HybridService.getHybridUi(_ctx, "file://android_asset/root.htm");
-//            }
-//        }, 5000);
-
-//        new Handler().postDelayed(new Runnable() {
-//            public void run() {
-////                Intent intent = new Intent(_ctx, HybridUi.class);
-////                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-////                startActivity(intent);
-//                HybridService.getHybridUi(_ctx, "file://android_asset/root.htm");
-//            }
-//        }, 8000);
-
-//        intent.putExtra("Topbar", Topbar);
-//        intent.putExtra("Mode", Mode);
-//        intent.putExtra("Address", Address);
-
-        //TODO start the background network status checker...
-//        new Handler().postDelayed(new Runnable() {
-//            public void run() {
-//                int pid = android.os.Process.myPid();
-//                Log.d(LOGTAG, "kill and quit pid=" + pid);
-//
-//                android.os.Process.killProcess(pid);
-//                System.exit(0);
-//            }
-//        }, (playMedia) ? 5000 : 3000);
-
     }
 }
--- proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/EntryActivity.java
+++ proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/EntryActivity.java
@@ -1,5 +1,30 @@
 package szu.bdi.hybrid.demo;
 
-public class EntryActivity extends szu.bdi.hybrid.core.HybridUi
-{
+import android.app.Activity;
+
+public class EntryActivity extends Activity {
+//    final private static String LOGTAG = "" + (new Object() {
+//        public String getClassName() {
+//            String clazzName = this.getClass().getName();
+//            return clazzName.substring(0, clazzName.lastIndexOf('$'));
+//        }
+//    }.getClassName());
+
+    protected void fwdToMain() {
+        DemoApp.main();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        fwdToMain();
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fwdToMain();
+        finish();
+    }
 }
