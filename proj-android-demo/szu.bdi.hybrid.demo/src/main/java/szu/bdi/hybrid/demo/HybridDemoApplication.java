26ba48a7b0148b951490a884198ca0030592953d
cmptech <wanjochan@gmail.com>
2016-06-26 12:29:19 +0800

    done before api_mapping

--- proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/HybridDemoApplication.java
+++ proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/HybridDemoApplication.java
@@ -1,13 +1,13 @@
 package szu.bdi.hybrid.demo;
 
-import android.app.Application;
 import android.util.Log;
 
+import szu.bdi.hybrid.core.HybridApplication;
 import szu.bdi.hybrid.core.HybridTools;
 
 //NOTES: must have an applicaton, otherwise data e.g.  app context maybe cleaned up by GC !!!!
 
-public class HybridDemoApplication extends Application {
+public class HybridDemoApplication extends HybridApplication {
     final private static String LOGTAG = "" + (new Object() {
         public String getClassName() {
             String clazzName = this.getClass().getName();
@@ -20,6 +20,28 @@ public class HybridDemoApplication extends Application {
         super.onCreate();
         Log.v(LOGTAG, "App.onCreate");
         HybridTools.setAppContext(getApplicationContext());
+
+        //TODO register a listenr to event "onKeyDown_LastAct"
     }
+    //TODO the global onKeyDown_LastAct Handler
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if (keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN) {
+//            if ((System.currentTimeMillis() - exitTime) > 2000) {
+//                Toast.makeText(getApplicationContext(), getText(R.string.system_exit), Toast.LENGTH_SHORT).show();
+//                exitTime = System.currentTimeMillis();
+//            } else {
+//                String filename = "StaticFileName";
+//                String field = "IfScreenCloseorAcDestroyHappened";
+//
+//                boolean flag1 = true;
+//                AppHelper.putSharePreBoolean(MainActivity.this, filename, field, flag1);
+//
+//                ActivityCollector.ExitApp();
+//            }
+//            return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
 }
 
