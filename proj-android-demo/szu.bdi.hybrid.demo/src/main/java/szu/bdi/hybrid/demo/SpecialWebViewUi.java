c61982f3bd779ba13783285992b7d9f90fe63619
cmptech <wanjochan@gmail.com>
2016-06-26 12:45:12 +0800

    moved tabor handling to HybridUi Class

---
+++ proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/SpecialWebViewUi.java
@@ -0,0 +1,21 @@
+package szu.bdi.hybrid.demo;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import szu.bdi.hybrid.core.WebViewUi;
+
+/**
+ * TODO want to do a special ui for fun only:
+ * <p/>
+ * 1, [height 38%] the control area
+ * 2, [height 62%] one is JsBridgeWebView
+ */
+public class SpecialWebViewUi extends WebViewUi {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
