2f2be50a50e7ea4e119adf6d34120405bc38af96
Wanjo Chan <wanjochan@gmail.com>
2016-08-29 00:49:47 +0800

    build.gradle

--- proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/HybridDemoApplication.java
+++ proj-android-demo/szu.bdi.hybrid.demo/src/main/java/szu/bdi/hybrid/demo/HybridDemoApplication.java
@@ -1,5 +1,6 @@
 package szu.bdi.hybrid.demo;
 
+import android.app.Application;
 import android.util.Log;
 
 import szu.bdi.hybrid.core.HybridApplication;
@@ -7,7 +8,10 @@ import szu.bdi.hybrid.core.HybridTools;
 
 //NOTES: must have an applicaton, otherwise data e.g.  app context maybe cleaned up by GC !!!!
 
-public class HybridDemoApplication extends HybridApplication {
+public class HybridDemoApplication
+        extends HybridApplication
+    //extends Application
+        {
     final private static String LOGTAG = "" + (new Object() {
         public String getClassName() {
             String clazzName = this.getClass().getName();
