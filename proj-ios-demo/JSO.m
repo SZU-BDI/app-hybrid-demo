81f35b086cbfdf07507b2c85e49ef36b310f7e2c
wanjochan <wanjochan@gmail.com>
2016-10-30 13:13:53 +0800

    WJC: 先放回双虎可用的版本。。。

---
+++ proj-ios-demo/JSO.m
@@ -0,0 +1,80 @@
+//
+//  JSO.m
+//  AsiaWeiLuy
+//
+//  Created by 双虎 on 16/8/30.
+//  Copyright © 2016年 Megadata. All rights reserved.
+//
+
+#import "JSO.h"
+
+// 抽象类
+@implementation JSO
+
+- (void)testAction{
+    
+    NSLog(@"JSO test");
+}
+
+@end
+
+// JSODictionary
+@implementation JSODictionary
+
+- (void)testAction{
+    
+    NSLog(@"修改父类方法 JSODictionary test");
+}
+
+@end
+
+// JSOArray
+@implementation JSOArray
+
+- (void)testAction{
+    
+    NSLog(@"修改父类方法 JSOArray test");
+}
+
+@end
+
+// JSONumber
+@implementation JSONumber
+
+- (void)testAction{
+    
+    NSLog(@"修改父类方法 JSONumber test");
+}
+
+@end
+
+// JSOString
+@implementation JSOString
+
+- (instancetype)init{
+    
+    self = [super init];
+    if (self) {
+     
+        NSLog(@"222222");
+    }
+    return self;
+}
+
+- (void)testAction{
+    
+    NSLog(@"修改父类方法 JSOString test");
+}
+
+@end
+
+// JSONull
+@implementation JSONull
+
+
+@end
+
+@implementation JSOBollean
+
+
+@end
