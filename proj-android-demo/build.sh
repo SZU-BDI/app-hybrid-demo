2ea10ae8d2a555a3d2357fbadf0d0be4652eb0eb
wanjochan <wanjochan@gmail.com>
2016-12-02 16:57:07 +0800

    WJC: build.sh

--- proj-android-demo/build.sh
+++ proj-android-demo/build.sh
@@ -1,14 +1,14 @@
 
 # check env var JAVA_HOME:
 if [ ! -n "$JAVA_HOME" ] ;then
-
-	echo ERROR: Need JAVA_HOME, for example:
-	echo export JAVA_HOME=\"/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home\"
-	exit;
-
+	export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home"
 fi
 
 if [ ! -x "$JAVA_HOME/bin/java" ] ;then
+
+	#echo ERROR: Need JAVA_HOME, for example:
+	#echo export JAVA_HOME=\"/Applications/Android Studio-2.app/Contents/jre/jdk/Contents/Home\"
+	#echo export JAVA_HOME=\"/Applications/Android Studio.app/Contents/jre/jdk/Contents/Home\"
 
 	echo cannot find the java in the $JAVA_HOME
 	exit;
@@ -26,9 +26,11 @@ echo "sdk.dir=$HOME/Library/Android/sdk" > local.properties
 # ===
 # gradlew
 
-#sh gradlew --stacktrace --info build
+sh gradlew --stacktrace --info build -x test
 
-sh gradlew build
+#gradle build -x test 
+
+#sh gradlew build
 
 # === build release
 #./gradlew assembleRelease
